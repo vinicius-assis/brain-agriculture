4212fc92ac503e03df403aac1d2bc4c34bef6a01
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-09-16 11:13:38 -0300

    feat: update input to work with react hook form and zod

--- src/app/components/Input/Input.test.tsx
+++ src/app/components/Input/Input.test.tsx
@@ -1,11 +1,99 @@
-import { screen, render } from "@testing-library/react";
-import Input from ".";
+import { render, screen } from "@testing-library/react";
+import Input from "./index";
+import { useForm, FieldError } from "react-hook-form";
+
+const MockFormWrapper = ({
+  children,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  children: (register: any) => JSX.Element;
+}) => {
+  const { register } = useForm();
+  return <form>{children(register)}</form>;
+};
 
 describe("#Input Suite", () => {
-  it("should render Input component", () => {
-    render(<Input label="Name" />);
+  it("should render Input component with a label", () => {
+    render(
+      <MockFormWrapper>
+        {(register) => <Input label="Name" register={register} name="name" />}
+      </MockFormWrapper>
+    );
 
-    const element = screen.getByTestId("input");
-    expect(element).toBeInTheDocument();
+    const labelElement = screen.getByLabelText("Name");
+    expect(labelElement).toBeInTheDocument();
+  });
+
+  it("should display the correct placeholder text", () => {
+    render(
+      <MockFormWrapper>
+        {(register) => (
+          <Input
+            label="Email"
+            placeholder="Enter your email"
+            register={register}
+            name="email"
+          />
+        )}
+      </MockFormWrapper>
+    );
+
+    const inputElement = screen.getByPlaceholderText("Enter your email");
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should display an error message if provided", () => {
+    // Mocking FieldErrors properly
+    const mockErrors = {
+      name: {
+        type: "required",
+        message: "Name is required",
+      } as FieldError, // Correctly cast the type here
+    };
+
+    render(
+      <MockFormWrapper>
+        {(register) => (
+          <Input
+            label="Name"
+            register={register}
+            name="name"
+            errors={mockErrors}
+          />
+        )}
+      </MockFormWrapper>
+    );
+
+    const errorMessage = screen.getByText("Name is required");
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("should render input with the correct type", () => {
+    render(
+      <MockFormWrapper>
+        {(register) => (
+          <Input
+            label="Password"
+            type="password"
+            register={register}
+            name="password"
+          />
+        )}
+      </MockFormWrapper>
+    );
+
+    const inputElement = screen.getByLabelText("Password");
+    expect(inputElement).toHaveAttribute("type", "password");
+  });
+
+  it("should not display an error message if no error is provided", () => {
+    render(
+      <MockFormWrapper>
+        {(register) => <Input label="Name" register={register} name="name" />}
+      </MockFormWrapper>
+    );
+
+    const errorMessage = screen.queryByText("Name is required");
+    expect(errorMessage).not.toBeInTheDocument();
   });
 });
--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -1,32 +1,50 @@
 import React from "react";
+import {
+  UseFormRegister,
+  FieldValues,
+  Path,
+  FieldErrors,
+} from "react-hook-form";
 
-interface IInputProps {
+interface IInputProps<T extends FieldValues> {
   label: string;
   className?: string;
   placeholder?: string;
   type?: string;
+  register: UseFormRegister<T>;
+  name: Path<T>;
+  errors?: FieldErrors<T>;
 }
 
-const Input = ({
+const Input = <T extends FieldValues>({
   label,
   className = "",
   placeholder = "",
   type = "text",
-}: IInputProps) => {
+  register,
+  name,
+  errors,
+}: IInputProps<T>) => {
   return (
     <div className={className} data-testid="input">
       <label
         className="block text-small-spacing font-normal mb-1 uppercase"
-        htmlFor="grid-first-name"
+        htmlFor={name}
       >
         {label}
       </label>
       <input
         className="appearance-none block w-full bg-off-white text-dark-green border-light rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-medium-green"
-        id="grid-first-name"
+        id={name}
         type={type}
         placeholder={placeholder}
+        {...register(name)}
       />
+      {errors && errors[name]?.message && (
+        <p className="text-red-500">
+          {errors[name]?.message as React.ReactNode}
+        </p>
+      )}
     </div>
   );
 };
