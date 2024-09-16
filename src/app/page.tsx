cc14cc6e8e313a9ffdb7ebeba35ae5c526e8c9ef
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-09-16 17:10:48 -0300

    refactor: add dispatch in dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
+"use client";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "./components/Button";
+import ChartCard from "./components/ChartCard";
+import Menu from "./components/Menu";
 import TotalCard from "./components/TotalCard";
+import { toggleForm } from "../../store/reducers/actions";
+import FarmForm from "./components/FarmForm";
+import { getFormState } from "../../store/reducers/selectors";
 
-export default function Home() {
+export default function Dashboard() {
+  const dispatch = useDispatch();
+  const showForm = useSelector(getFormState);
+  const handleCloseForm = () => dispatch(toggleForm());
+
   return (
-    <div>
-      <TotalCard title="Total Orders" value="84,382" percent="36" />
-      <TotalCard title="Total Sales" value="$2,485" percent="14" decrease />
+    <div className="p-8 h-[calc(100vh-104px)] overflow-auto">
+      <Menu />
+      <FarmForm show={showForm} onClose={handleCloseForm} />
+      <Button onClick={() => dispatch(toggleForm())} className="mx-auto">
+        + New Profile
+      </Button>
+      <div className="w-full flex gap-6 mt-9 overflow-auto pb-2">
+        <TotalCard title="Total Profiles" value="20" percent="20%" />
+        <TotalCard title="Total Farms" value="50" percent="14%" />
+        <TotalCard title="Total Area" value="50m2" percent="14%" />
+      </div>
+      <div className="mt-10">
+        <ChartCard
+          title="Total Crops"
+          labels={["Soybean", "Corn", "Cotton", "Coffee", "Sugarcane"]}
+          series={[10, 20, 32, 40, 11]}
+        />
+      </div>
+      <div className="mt-10">
+        <ChartCard
+          title="Total Crops"
+          labels={["Soybean", "Corn", "Cotton", "Coffee", "Sugarcane"]}
+          series={[10, 20, 32, 40, 11]}
+        />
+      </div>
     </div>
   );
 }
