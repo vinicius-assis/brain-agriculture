84ee15a2396283c1b7528bf4d5d1d7d506e0e4b6
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-09-17 21:24:06 -0300

    feat: adjust validation

--- src/app/components/FarmForm/validationSchema.ts
+++ src/app/components/FarmForm/validationSchema.ts
@@ -1,38 +1,40 @@
 import { z } from "zod";
 
-const cpfOrCnpjRegex = /^(?:\d{11}|\d{14})$/; // Basic validation regex for CPF/CNPJ
+// Regex CPF e CNPJ
+const cpfOrCnpjRegex =
+  /^(?:\d{3}\.\d{3}\.\d{3}-\d{2}|\d{11}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}|\d{14})$/;
 
 export const schema = z
   .object({
     document: z
       .string()
       .regex(cpfOrCnpjRegex, "Document is invalid")
-      .min(11, "CPF must be 11 digits")
-      .max(14, "CNPJ must be 14 digits"),
+      .transform((val) => val.replace(/\D/g, ""))
+      .refine((val) => val.length === 11 || val.length === 14, {
+        message: "Document must be 11 or 14 digits",
+      }),
     name: z.string().nonempty("Producer Name is required"),
     farmName: z.string().nonempty("Farm Name is required"),
     city: z.string().nonempty("City is required"),
     state: z.string().nonempty("State is required"),
-    totalArea: z.coerce
+    totalArea: z
       .string()
-      .min(0, "Total area must be a positive number")
-      .transform((val) => parseFloat(val)),
-    cultivableArea: z.coerce
+      .transform((val) => parseFloat(val))
+      .refine((val) => val >= 0, "Total area must be a positive number"),
+    cultivableArea: z
       .string()
-      .min(0, "Cultivable area must be a positive number")
-      .transform((val) => parseFloat(val)),
-    vegetationArea: z.coerce
+      .transform((val) => parseFloat(val))
+      .refine((val) => val >= 0, "Cultivable area must be a positive number"),
+    vegetationArea: z
       .string()
-      .min(0, "Vegetation area must be a positive number")
-      .transform((val) => parseFloat(val)),
+      .transform((val) => parseFloat(val))
+      .refine((val) => val >= 0, "Vegetation area must be a positive number"),
     crops: z
       .array(z.enum(["Soybean", "Corn", "Cotton", "Coffee", "Sugarcane"]))
       .nonempty("Select at least one crop"),
   })
   .refine(
-    (data) => {
-      return data.cultivableArea + data.vegetationArea <= data.totalArea;
-    },
+    (data) => data.cultivableArea + data.vegetationArea <= data.totalArea,
     {
       message:
         "The sum of cultivable area and vegetation area cannot be greater than the total area of the farm",
